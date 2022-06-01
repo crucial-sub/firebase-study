6f3c360517e7a5231f6f6101965d466fc70372e4
crucial-sub <jssub940@gmail.com>
2022-06-01 23:58:41 +0900

    [Feat] Todo delete 기능

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,12 +1,32 @@
-import { ListItem, ListItemText } from "@mui/material";
+import { IconButton, ListItem, ListItemText } from "@mui/material";
 import moment from "moment";
 import { TodoType } from "../types/todo";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Todo = ({ id, timestamp, title, detail }: TodoType) => {
+  const deleteTodo = async (id, e) => {
+    e.stopPropagation();
+    const docRef = doc(db, "todo", id);
+    await deleteDoc(docRef);
+    showAlert("error", `Todo with id ${id} deleted successfully`);
+  };
   return (
     <ListItem
       sx={{ mt: 3, boxShadow: 3 }}
       style={{ backgroundColor: "#FAFAFA" }}
+      secondaryAction={
+        <>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </>
+      }
     >
       <ListItemText
         primary={title}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import { Container } from "@mui/material";
+import { Alert, Container, Snackbar } from "@mui/material";
 import type { NextPage } from "next";
+import { useState } from "react";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
 
 const Home: NextPage = () => {
+  const [open, setOpen] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+  const showAlert = (type, msg) => {
+    setAlertType(type);
+    setAlertMessage(msg);
+    setOpen(true);
+  };
   return (
     <div>
       <Container maxWidth="sm">
         <TodoForm />
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity={alertType}
+            sx={{ width: "100%" }}
+          >
+            {alertMessage}
+          </Alert>
+        </Snackbar>
         <TodoList />
       </Container>
     </div>
